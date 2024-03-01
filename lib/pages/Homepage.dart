3a237e1ee55f02867f59d77a50218e676602434a
hasib94badhon <hasib.badhon1234@gmail.com>
2024-03-02 02:24:30 +0600

    1st_march_dev

---
+++ lib/pages/Homepage.dart
@@ -0,0 +1,125 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:namer_app/widgets/DashBoardWidget.dart';
+
+
+import 'package:namer_app/widgets/HomeAppBar.dart';
+import 'package:namer_app/widgets/ItemsWidget.dart';
+
+
+class HomePage extends StatelessWidget {
+  
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: ListView(children: [
+        HomeAppBar(),
+        Container(
+          // height: 500,
+          padding: EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(
+            color:Color(0xFFEDECF2),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(35),
+              topRight: Radius.circular(35),
+            )
+          ),
+          child: Column(
+            children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.only(left: 5),
+                  height: 50,
+                  width: 300,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Look here",
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Icon(
+                  Icons.camera_alt,
+                  size: 27,
+                  color: Color(0xFF4C53A5),
+                ),
+
+              ],),
+            
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 10,
+              ),
+              child: Text(
+                "DashBoard",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0XFF4C53A5)
+                ),
+              ),
+            ),
+            //DashBoard
+            DashBoardWiget(),
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: EdgeInsets.symmetric(vertical: 20,horizontal: 10),
+              child: Text(
+                "Top Rated",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF4C53A5),
+                ),
+              ),
+            ),
+
+            //Items Widget
+            ItemsWidget(),
+
+            
+          ],),
+        )
+        ],
+      ),
+
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        onTap: (index){},
+        height: 70,
+        color: Color(0xFF4C53A5),
+        items: [
+          Icon(
+            Icons.home,
+            size:30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.ad_units_sharp,
+            size:30,
+            color: Colors.white,
+          ),
+          Icon(
+            Icons.door_front_door,
+            size:30,
+            color: Colors.white,
+          ),
+
+      ],
+      ),
+    );
+  }
+  
+}
---
+++ lib/pages/cartPage.dart
@@ -0,0 +1,67 @@
+import 'dart:js_interop';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:namer_app/widgets/CartAppBar.dart';
+import 'package:namer_app/widgets/CartBottomNavbar.dart';
+import 'package:namer_app/widgets/CartItemSamples.dart';
+
+
+class cartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          CartAppBar(),
+
+          Container(
+            //temporary height
+            height: 1000,
+            padding: EdgeInsets.only(top: 25),
+            decoration: BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(
+              children: [
+              CartItemsSamples(),
+              Container(
+                //
+                //
+                margin: EdgeInsets.symmetric(vertical: 20,horizontal: 15),
+                padding: EdgeInsets.all(10),
+                child: Row(children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color:Color(0xFF4C53A5),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Icon(
+                     Icons.add,
+                     color: Colors.white,
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    'Add Coupon Here',
+                    style: TextStyle(
+                      color: Color(0xFF4C53A5),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),)
+                ],),
+              ),
+            ],
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: CartBottomNavbar(),
+    );
+  }
+}
