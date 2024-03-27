f4acae7f8def57e827cc636a7c6279e9d1a3222f
hasib94badhon <hasib.badhon1234@gmail.com>
2024-03-28 01:09:22 +0300

    profile

--- lib/screens/advert_screen.dart
+++ lib/screens/advert_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fan_carousel_image_slider/fan_carousel_image_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:namer_app/widgets/advert_details_popup.dart';
 
@@ -17,17 +18,17 @@ class AdvertScreen extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(10),
           child: SafeArea(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 SizedBox(
-                  height: 250,
+                  height: 220,
                   width: MediaQuery.of(context).size.width,
                   child: FanCarouselImageSlider(
-                      sliderHeight: 230,
+                      sliderHeight: 200,
                       autoPlay: true,
                       imagesLink: images,
                       isAssets: true),
@@ -38,16 +39,15 @@ class AdvertScreen extends StatelessWidget {
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(height: 30),
+                        SizedBox(height: 10),
                         Text(
                           "Rafiq Ahmed",
                           style: TextStyle(
                             color: Colors.black87,
                             fontWeight: FontWeight.w900,
-                            fontSize: 25,
+                            fontSize: 23,
                           ),
                         ),
-                        SizedBox(height: 5),
                         Text(
                           "Electrician",
                           style: TextStyle(
@@ -58,17 +58,45 @@ class AdvertScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                    Text(
-                      "\$300",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Colors.amber,
+
+                    //This is a button to edit profile and show phone
+                    Container(
+                      //color: Colors.amber,
+                      margin: EdgeInsets.only(top: 30, right: 10),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "Edit Profile",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.amber,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "+01*******33",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     )
                   ],
                 ),
-                SizedBox(height: 10),
+
+                //This a rating star
                 Align(
                   alignment: Alignment.centerLeft,
                   child: RatingBar.builder(
@@ -87,10 +115,20 @@ class AdvertScreen extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 10),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 2,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.grey),
+                  ),
+                ),
+                SizedBox(height: 5),
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    "slkjdfgnpadfadfn odfjaf aspefjigsd eroghasdfv efnvs9dhfv eriughsdfvnadf adfhvjknsefg oidafjvsdfng.",
+                    "As an electrician, I excel in fixing electrical problems and ensuring safety. I'm skilled in wiring buildings and troubleshooting issues efficiently. My expertise extends to various settings, from homes to commercial spaces. I pride myself on my attention to detail and ability to deliver reliable solutions. Clients trust me to keep their electrical systems running smoothly.",
+                    maxLines: 3,
+                    overflow: TextOverflow.fade,
                     style: TextStyle(
                       color: Colors.black54,
                       fontSize: 16,
@@ -98,28 +136,69 @@ class AdvertScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height: 30),
+                SizedBox(height: 5),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 2,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.grey),
+                  ),
+                ),
+                SizedBox(height: 5),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Container(
-                      height: 60,
-                      width: 60,
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.call_end_rounded,
-                          color: Colors.white,
+                      margin: EdgeInsets.only(left: 150),
+                      child: Text(
+                        "Albumn",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
-                    AdvertDetailsPopup(),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 2,color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.transparent,
+                      ),
+                      width: 100,
+                      height: 40,
+                      child: Row(
+                        children: [
+                          Text(
+                            "Upload",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w200,
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.upload,
+                            size: 20,
+                            color: Colors.green,
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
-                SizedBox(height: 30),
+
+                SizedBox(height: 5),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 2,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.grey),
+                  ),
+                ),
+                SizedBox(height: 5),
               ],
             ),
           ),
