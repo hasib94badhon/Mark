2606da81c046abd6659c4a887a8c18fa1bc1487f
hasib94badhon <hasib.badhon1234@gmail.com>
2024-04-12 02:12:45 +0600

    favorite page update

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:aaram_bd/screens/advert_screen.dart';
 import 'package:http/http.dart' as http;
+
 class FavoriteScreen extends StatefulWidget {
-  
-  
   @override
   State<FavoriteScreen> createState() => _FavoriteScreenState();
 }
 
 class _FavoriteScreenState extends State<FavoriteScreen> {
-  
   List<String> tabs = ["All", "Category", "Top", "Recommended"];
 
   List imageList = [
@@ -50,109 +47,95 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Padding(
-            padding: EdgeInsets.only(left: 15, right: 15, top: 20),
+            padding: EdgeInsets.all(15),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-
-                    //Search button
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      height: 50,
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      decoration: BoxDecoration(
-                        color: Colors.black12.withOpacity(0.05),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          // BoxShadow(
-                          //   color: Colors.black12,
-                          //   blurRadius: 4,
-                          //   spreadRadius: 2,
-                          // ),
-                        ],
-                      ),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Colors.blue.shade800,
-                          ),
-                          border: InputBorder.none,
-                          label: Text(
-                            "Find your service",
-                            style: TextStyle(),
-                          ),
-                        ),
-                      ),
-                    ),
-                    //NotificationICOn
-                    Container(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width / 6,
-                      decoration: BoxDecoration(
-                        color: Colors.black12.withOpacity(0.05),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.notifications,
-                          color: Colors.blue.shade800,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 10),
-                //aaramBD logo + Banner
+                // Search Button
                 Container(
-                  height: 150,
-                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.only(bottom: 10),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
                   decoration: BoxDecoration(
-                    color: Color(0xFFFFF0DD),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 1,
-                            spreadRadius: 2,
-                          ),
-                        ],
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Image.asset("images/AaramBD_logo.jpg",
-                  fit: BoxFit.cover,),
+                  child: Row(
+                    children: [
+                      Icon(Icons.search, color: Colors.blue),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Find your service',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 10),
 
-                //Title Category
+                // // Fixed Banner
+                // Container(
+                //   margin: EdgeInsets.symmetric(vertical: 10),
+                //   height: 150,
+                //   decoration: BoxDecoration(
+                //     color: Colors.grey.shade300,
+                //     borderRadius: BorderRadius.circular(20),
+                //   ),
+                //   child: Image.asset(
+                //     "images/AaramBD_logo.jpg",
+                //     //fit: BoxFit.cover,
+                //   ),
+                // ),
+
+                // Sliding Show
                 SizedBox(
-                  height: 60,
+                  height: 150,
+                  child: ListView.builder(
+                    itemCount: imageList.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: 150,
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.grey.shade300),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset(
+                            imageList[index],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                // Tabs
+                SizedBox(
+                  height: 70,
                   child: ListView.builder(
                     shrinkWrap: true,
                     scrollDirection: Axis.horizontal,
                     itemCount: tabs.length,
                     itemBuilder: (context, index) {
-                      return FittedBox(
-                        child: Container(
-                          height: 40,
-                          margin: EdgeInsets.all(5),
-                          padding: EdgeInsets.only(left: 05, right: 05),
-                          decoration: BoxDecoration(
-                            color: Colors.black12.withOpacity(0.05),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Center(
-                            child: FittedBox(
-                              child: Text(
-                                tabs[index],
-                                style: TextStyle(
-                                  color: Colors.black38,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                ),
-                              ),
+                      return Container(
+                        margin: EdgeInsets.only(right: 10, top: 10, left: 10, bottom: 10),
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            tabs[index],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
                             ),
                           ),
                         ),
@@ -160,104 +143,73 @@ class _FavoriteScreenState extends State<FavoriteScreen> {
                     },
                   ),
                 ),
-                SizedBox(height: 20),
 
-                //sliding show
-                Container(
-                  height: 1000,
+                // Product List
+                SizedBox(
+                  height: 500,
                   child: ListView.builder(
-                    itemCount: imageList.length,
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
+                    itemCount: productTitles.length,
                     itemBuilder: (context, index) {
-                      return Column(
-                        children: [   
-                          
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              border: Border.all(width: 2,color: Colors.black),
-                              borderRadius: BorderRadius.circular(30),
+                      return Container(
+                        margin: EdgeInsets.symmetric(vertical: 5),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade200,
+                          border: Border.all(width: 1, color: Colors.black),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 100,
+                              height: 100,
+                              margin: EdgeInsets.only(right: 20),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(color: Colors.grey),
+                              ),
+                              child: Image.asset(
+                                imageList[index],
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                            width: 300,
-                            margin: EdgeInsets.only(right: 15),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: 150,
-                                  width: 150,
-                                  child: Stack(
-                                    children: [
-                                      InkWell(
-                                        onTap: () {},
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.circular(20),
-                                          child: Center(
-                                            child: Image.asset(
-                                              imageList[index],
-                                              fit: BoxFit.cover,
-                                              height: 130,
-                                              width: 130,
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    productTitles[index],
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 8),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                  SizedBox(height: 5),
+                                  Text(
+                                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  SizedBox(height: 5),
+                                  Row(
                                     children: [
+                                      Icon(Icons.star, color: Colors.amber),
+                                      Text(reviews[index]),
+                                      SizedBox(width: 10),
                                       Text(
-                                        productTitles[index],
+                                        prices[index],
                                         style: TextStyle(
-                                          fontSize: 18,
                                           fontWeight: FontWeight.bold,
+                                          color: Colors.blue,
                                         ),
-                                      ),
-                                      SizedBox(height: 10),
-                                      SizedBox(
-                                        width: 120,
-                                        child: Text(
-                                          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-                                          maxLines: 4,
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ),
-                                      SizedBox(height: 10),
-                                      Row(
-                                        children: [
-                                          Icon(
-                                            Icons.star,
-                                            color: Colors.amber,
-                                            size: 22,
-                                          ),
-                                          Text(
-                                            '(' + reviews[index] + ')',
-                                          ),
-                                          SizedBox(width: 10),
-                                          Text(
-                                            prices[index],
-                                            style: TextStyle(
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.amber,
-                                            ),
-                                          ),
-                                        ],
                                       ),
                                     ],
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                          SizedBox(height: 20),
-                        ],
+                          ],
+                        ),
                       );
                     },
                   ),
